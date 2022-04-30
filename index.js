a27573b19d4793b770f09f07efdb904ea17030bd
Mohammad Ali <itsproali@gmail.com>
2022-05-01 00:59:44 +0600

    Load 6 Items for home page

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://itsproali:${process.env.DB_PASSWORD}@bikecluster.vxpkk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const itemCollection = client.db("SuperBike").collection("items");
+
+    //   Load 6 Items
+    app.get("/items/6", async (req, res) => {
+      const query = {};
+      const cursor = itemCollection.find(query).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+    // client.close()
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Yay Super Bike server is running smoothly !!!");
