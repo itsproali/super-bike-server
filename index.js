3a7559d714ad08af2b67a27f4156c47409ca8219
Mohammad Ali <itsproali@gmail.com>
2022-12-10 14:50:49 +0600

    🔥 404 Issue resolved

--- index.js
+++ index.js
@@ -37,158 +37,152 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-const run = async () => {
-  try {
-    await client.connect();
-    const itemCollection = client.db("SuperBike").collection("items");
-    const memberCollection = client.db("SuperBike").collection("members");
-
-    // Simple route
-    app.get("/simple", async (req, res) => {
-      res.send("Simple Route Working");
-    });
-
-    // Count Product
-    app.get("/item-count", async (req, res) => {
-      const count = await itemCollection.estimatedDocumentCount();
-      res.send({ count });
-    });
-
-    //   Load All Items
-    app.get("/items", async (req, res) => {
-      const query = {};
-      const cursor = itemCollection.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
-    });
-
-    //   Load 6 Items
-    app.get("/items/6", async (req, res) => {
-      const query = {};
-      const cursor = itemCollection.find(query).limit(6);
-      const result = await cursor.toArray();
-      res.send(result);
-    });
-
-    // Load specific data by id
-    app.get("/item/:id", async (req, res) => {
-      const query = { _id: ObjectId(req.params.id) };
-      const result = await itemCollection.findOne(query);
-      res.send(result);
-    });
-
-    // Update Quantity & Sold
-    app.put("/item/:id", async (req, res) => {
-      const quantity = req.body.quantity;
-      const sold = req.body.sold;
-      const data = { quantity: quantity - 1, sold: sold + 1 };
-      const query = { _id: ObjectId(req.params.id) };
-      const options = { upsert: true };
-      const updateDoc = {
-        $set: data,
-      };
-      const updateQuantity = await itemCollection.updateOne(
-        query,
-        updateDoc,
-        options
-      );
-      res.send(updateQuantity);
-    });
-
-    // Update Stock
-    app.put("/stockItem/:id", async (req, res) => {
-      const quantity = req.body.quantity;
-      const stock = req.body.stock;
-      const data = { quantity: quantity + stock };
-      const query = { _id: ObjectId(req.params.id) };
-      const options = { upsert: true };
-      const updateDoc = {
-        $set: data,
-      };
-      const updateQuantity = await itemCollection.updateOne(
-        query,
-        updateDoc,
-        options
-      );
-      res.send(updateQuantity);
-    });
-
-    // Update Item Details
-    app.put("/edit/:id", async (req, res) => {
-      const item = req.body.item;
-      const query = { _id: ObjectId(req.params.id) };
-      const options = { upsert: true };
-      const updateDoc = {
-        $set: item,
-      };
-      const updateQuantity = await itemCollection.updateOne(
-        query,
-        updateDoc,
-        options
-      );
-      res.send(updateQuantity);
-    });
-
-    // Add a new Product
-    app.post("/add", async (req, res) => {
-      const item = req.body.item;
-      const result = await itemCollection.insertOne(item);
-      res.send(result);
-    });
-
-    // Delete a Product
-    app.delete("/delete/:id", async (req, res) => {
-      const query = { _id: ObjectId(req.params.id) };
-      const result = await itemCollection.deleteOne(query);
-      res.send(result);
-    });
-
-    // My Items
-    app.get("/my-items", async (req, res) => {
-      const uid = req.query.uid;
-      // const decodedUid = req.decoded.uid;
-      // if (uid === decodedUid) {
-      const query = { uid: uid };
-      const cursor = itemCollection.find(query);
-      const myItems = await cursor.toArray();
-      res.send(myItems);
-      // } else {
-      //   res.status(403).send({ message: "Forbidden Access" });
-      // }
-    });
-
-    // Load All Members
-    app.get("/members", async (req, res) => {
-      const query = {};
-      const cursor = memberCollection.find(query);
-      const members = await cursor.toArray();
-      res.send(members);
-    });
-
-    // get Token
-    app.post("/getToken", async (req, res) => {
-      const user = req.body.userId;
-      const accessToken = jwt.sign({ user }, process.env.ACCESS_SECRET, {
-        expiresIn: "1d",
-      });
-      res.send({ accessToken });
-    });
-  } finally {
-    // client.close()
-  }
+const dbConnect = async () => {
+  await client.connect();
 };
 
-// // Default Route
+// Call the run function
+dbConnect().catch(console.dir);
+
+// DB Collections
+const itemCollection = client.db("SuperBike").collection("items");
+const memberCollection = client.db("SuperBike").collection("members");
+
+// Default Route
 app.get("/", async (req, res) => {
   res.send("Yay Super Bike server is running smoothly !!!");
 });
 
-app.get("/checker", async (req, res) => {
-  res.send("Checker is working properly");
+// Simple route
+app.get("/simple", async (req, res) => {
+  res.send("Simple Route Working");
 });
 
-// Call the run function
-run().catch(console.dir);
+// Count Product
+app.get("/item-count", async (req, res) => {
+  const count = await itemCollection.estimatedDocumentCount();
+  res.send({ count });
+});
+
+//   Load All Items
+app.get("/items", async (req, res) => {
+  const query = {};
+  const cursor = itemCollection.find(query);
+  const result = await cursor.toArray();
+  res.send(result);
+});
+
+//   Load 6 Items
+app.get("/items/6", async (req, res) => {
+  const query = {};
+  const cursor = itemCollection.find(query).limit(6);
+  const result = await cursor.toArray();
+  res.send(result);
+});
+
+// Load specific data by id
+app.get("/item/:id", async (req, res) => {
+  const query = { _id: ObjectId(req.params.id) };
+  const result = await itemCollection.findOne(query);
+  res.send(result);
+});
+
+// Update Quantity & Sold
+app.put("/item/:id", async (req, res) => {
+  const quantity = req.body.quantity;
+  const sold = req.body.sold;
+  const data = { quantity: quantity - 1, sold: sold + 1 };
+  const query = { _id: ObjectId(req.params.id) };
+  const options = { upsert: true };
+  const updateDoc = {
+    $set: data,
+  };
+  const updateQuantity = await itemCollection.updateOne(
+    query,
+    updateDoc,
+    options
+  );
+  res.send(updateQuantity);
+});
+
+// Update Stock
+app.put("/stockItem/:id", async (req, res) => {
+  const quantity = req.body.quantity;
+  const stock = req.body.stock;
+  const data = { quantity: quantity + stock };
+  const query = { _id: ObjectId(req.params.id) };
+  const options = { upsert: true };
+  const updateDoc = {
+    $set: data,
+  };
+  const updateQuantity = await itemCollection.updateOne(
+    query,
+    updateDoc,
+    options
+  );
+  res.send(updateQuantity);
+});
+
+// Update Item Details
+app.put("/edit/:id", async (req, res) => {
+  const item = req.body.item;
+  const query = { _id: ObjectId(req.params.id) };
+  const options = { upsert: true };
+  const updateDoc = {
+    $set: item,
+  };
+  const updateQuantity = await itemCollection.updateOne(
+    query,
+    updateDoc,
+    options
+  );
+  res.send(updateQuantity);
+});
+
+// Add a new Product
+app.post("/add", async (req, res) => {
+  const item = req.body.item;
+  const result = await itemCollection.insertOne(item);
+  res.send(result);
+});
+
+// Delete a Product
+app.delete("/delete/:id", async (req, res) => {
+  const query = { _id: ObjectId(req.params.id) };
+  const result = await itemCollection.deleteOne(query);
+  res.send(result);
+});
+
+// My Items
+app.get("/my-items", async (req, res) => {
+  const uid = req.query.uid;
+  // const decodedUid = req.decoded.uid;
+  // if (uid === decodedUid) {
+  const query = { uid: uid };
+  const cursor = itemCollection.find(query);
+  const myItems = await cursor.toArray();
+  res.send(myItems);
+  // } else {
+  //   res.status(403).send({ message: "Forbidden Access" });
+  // }
+});
+
+// Load All Members
+app.get("/members", async (req, res) => {
+  const query = {};
+  const cursor = memberCollection.find(query);
+  const members = await cursor.toArray();
+  res.send(members);
+});
+
+// get Token
+app.post("/getToken", async (req, res) => {
+  const user = req.body.userId;
+  const accessToken = jwt.sign({ user }, process.env.ACCESS_SECRET, {
+    expiresIn: "1d",
+  });
+  res.send({ accessToken });
+});
 
 app.listen(port, () => {
   console.log("My super server is running on: ", port);
